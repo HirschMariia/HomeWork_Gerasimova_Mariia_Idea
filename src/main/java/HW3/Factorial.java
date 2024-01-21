96214bb6ecd475c2aea26a751736a06a48cb9cd3
Mariia <hirsch.mariia@icloud.com>
2024-01-21 23:34:34 +0100

    Third Homework 21.01.2024

--- src/main/java/HW3/Factorial.java
+++ src/main/java/HW3/Factorial.java
@@ -1,57 +1,58 @@
 package HW3;
 
-public class Factorial {
-    public static void main(String[] args) {
-        Factorial f = new Factorial();
 
-        System.out.println(f.factorialFor(5));
+    public class Factorial {
+        public static void main(String[] args) {
+            HW3.Factorial f = new HW3.Factorial();
 
-        System.out.println(f.factorialWhile(6));
+            System.out.println(f.factorialFor(5));
 
-        System.out.println(f.factorialDoWhile(7));
-}
+            System.out.println(f.factorialWhile(6));
 
-    public int factorialFor(int number) {
-        if (number <= 0) {
-            return 0;
+            System.out.println(f.factorialDoWhile(7));
         }
 
-        int result = 1;
-        for (int i = 1; i <= number; i++) {
-            result *= i;
+        public int factorialFor(int number) {
+            if (number <= 0) {
+                return 0;
+            }
+
+            int result = 1;
+            for (int i = 1; i <= number; i++) {
+                result *= i;
+            }
+
+            return result;
         }
 
-        return result;
+        public int factorialWhile(int number) {
+            if (number <= 0) {
+                return 0;
+            }
+
+            int result = 1;
+            int i = 1;
+            while(i <= number) {
+                result *= i;
+                i++;
+            }
+
+            return result;
+        }
+
+        public int factorialDoWhile(int number) {
+            if (number <= 0) {
+                return 0;
+            }
+
+            int result = 1;
+            int i = 1;
+
+            do {
+                result *= i;
+                i++;
+            } while(i <= number);
+
+            return result;
+        }
     }
-
-    public int factorialWhile(int number) {
-        if (number <= 0) {
-            return 0;
-        }
-
-        int result = 1;
-        int i = 1;
-        while(i <= number) {
-            result *= i;
-            i++;
-        }
-
-        return result;
-    }
-
-    public int factorialDoWhile(int number) {
-        if (number <= 0) {
-            return 0;
-        }
-
-        int result = 1;
-        int i = 1;
-
-        do {
-            result *= i;
-            i++;
-        } while(i <= number);
-
-        return result;
-    }
-}
